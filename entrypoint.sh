3dc808fd09e300084d34cf9bb35ee3be3ba835d6
Joao Figueiredo <joao.figueiredo@jfig.net>
2023-12-23 21:33:25 +0000

    added command line option to listen to connections

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # Start gpsd and forward arguments
 # Replace /dev/ttyUSB0 with your GPS device path
-exec gpsd -N -D3 -F /var/run/gpsd.sock /dev/ttyUSB0 "$@"
+exec gpsd -GNn -D3 -F /var/run/gpsd.sock /dev/ttyUSB0 "$@"
