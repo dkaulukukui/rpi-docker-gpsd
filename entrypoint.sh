14ccda72bc9d74774bf307c7bb02cc571fa81abe
Donald Kaulukukui <35284414+dkaulukukui@users.noreply.github.com>
2025-05-06 16:29:30 -1000

    Update entrypoint.sh
    updated to map to serial0 (GPIO UART)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Start gpsd and forward arguments
-# Replace /dev/ttyUSB0 with your GPS device path
-exec gpsd -GNn -D3 -F /var/run/gpsd.sock /dev/ttyUSB0 "$@"
+# Replace /dev/ttyUSB0 with your GPS device path, serial0 is the GPIO UART
+exec gpsd -GNn -D3 -F /var/run/gpsd.sock /dev/serial0 "$@"
